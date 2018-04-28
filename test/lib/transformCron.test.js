6d235df284011da463bb0dcb9c5621ec8e903c9e
fjahja <fjahja@oath.com>
2018-04-27 17:30:18 -0700

    feat(688): Add lib function to transform cron expression

---
+++ test/lib/transformCron.test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { assert } = require('chai');
+const transformCron = require('../../lib/transformCron.js');
+const hash = require('string-hash');
+
+const evaluateHash = (jobId, min, max) => (hash(jobId) % ((max + 1) - min)) + min;
+
+describe('transformCron', () => {
+    const jobId = '123';
+
+    // Evaluate the hashes for the default minutes and hours field
+    const minutesHash = evaluateHash(jobId, 0, 59);
+    const hoursHash = evaluateHash(jobId, 0, 23);
+
+    it('should throw if the cron expession does not have 5 fields', () => {
+        let cron;
+        // 6 fields
+
+        cron = '1 2 3 4 5 6';
+        assert.throws(() => transformCron(cron, jobId),
+            Error, '1 2 3 4 5 6 does not have exactly 5 fields');
+
+        // 4 fields
+        cron = '1 2 3 4';
+        assert.throws(() => transformCron(cron, jobId),
+            Error, '1 2 3 4 does not have exactly 5 fields');
+    });
+
+    it('should transform a cron expression with valid H symbol(s)', () => {
+        let cron;
+
+        // H * * * *
+        cron = 'H * * * *';
+        assert.deepEqual(transformCron(cron, jobId), `${minutesHash} * * * *`);
+
+        // * H/2 * * *
+        cron = '* H/2 * * *';
+        assert.deepEqual(transformCron(cron, jobId), `${minutesHash} ${hoursHash}/2 * * *`);
+
+        // * H(0-5) * * *
+        cron = '* H(0-5) * * *';
+        assert.deepEqual(transformCron(cron, jobId),
+            `${minutesHash} ${evaluateHash(jobId, 0, 5)} * * *`);
+    });
+
+    it('should throw if the cron expression has an invalid range value', () => {
+        const cron = '* H(99-100) * * *';
+
+        assert.throws(() => transformCron(cron, jobId),
+            Error, 'H(99-100) has an invalid range, expected range 0-23');
+    });
+});
