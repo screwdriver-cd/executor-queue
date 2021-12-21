b1ad5514105b7c808c771de56b5d39b5f81f72aa
Yoshika Shota <y.shota00@gmail.com>
2021-12-22 04:10:19 +0900

    fix(1500): add unzipArtifacts method (#97)

--- index.js
+++ index.js
@@ -134,6 +134,25 @@ class ExecutorQueue extends Executor {
     }
 
     /**
+     * Unzip the ZIP of artifacts
+     * @method _unzipArtifacts
+     * @param  {Object}  config           Configuration
+     * @param  {Number}  config.buildId   Unique ID for a build
+     * @param  {String}  config.token     JWT to act on behalf of the build
+     * @return {Promise}
+     */
+    async _unzipArtifacts(config) {
+        const options = {
+            path: '/v1/queue/message?type=unzip',
+            method: 'POST'
+        };
+
+        logger.info(`${options.method} ${options.path} for artifacts of build:${config.buildId}`);
+
+        return this.api(config, options);
+    }
+
+    /**
      * Starts a new build in an executor
      * @async  _start
      * @param  {Object} config               Configuration
